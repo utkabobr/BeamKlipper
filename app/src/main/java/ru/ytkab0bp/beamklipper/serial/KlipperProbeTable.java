1e9f3c1cae6d974478d58dc322ff9369b31037fe
YTKAB0BP <utkabobr@utkacraft.ru>
2024-07-01 19:23:13 +0300

    Add 0xDD8/0x3701 to known CH340 devices

--- app/src/main/java/ru/ytkab0bp/beamklipper/serial/KlipperProbeTable.java
+++ app/src/main/java/ru/ytkab0bp/beamklipper/serial/KlipperProbeTable.java
@@ -1,6 +1,7 @@
 package ru.ytkab0bp.beamklipper.serial;
 
 import com.hoho.android.usbserial.driver.CdcAcmSerialDriver;
+import com.hoho.android.usbserial.driver.Ch34xSerialDriver;
 import com.hoho.android.usbserial.driver.ProbeTable;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 
@@ -13,6 +14,7 @@ public class KlipperProbeTable extends ProbeTable {
         if (mInstance == null) {
             mInstance = UsbSerialProber.getDefaultProbeTable();
             mInstance.addProduct(0x1D50, 0x614E, CdcAcmSerialDriver.class);
+            mInstance.addProduct(0xDD8, 0x3701, Ch34xSerialDriver.class);
         }
         return mInstance;
     }
